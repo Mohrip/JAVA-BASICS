59269ad6f9a30e10a452e598d8f3aff63181d857
mohrip <m25srt@gmail.com>
2025-01-20 12:22:29 +0300

    ""

--- Basics/Datatypes.java
+++ Basics/Datatypes.java
@@ -5,8 +5,12 @@ public class Datatypes {
     public static void main(String[] args) {
     int a = 10;
     int b = 20;
+    int x = 30;
+    int y = 40;
+    float g = 10.5f;
     int c = a + b;
     int z = a * b;
+    float d = (a + b) / x + (y * g); 
 
     String none = "Hello World";
     String Fname = "John";
@@ -19,6 +23,7 @@ public class Datatypes {
         System.out.println(FullName);
         System.out.println(Fname + "" + Lname);
         System.out.println(isTrue);
+        System.out.println("this is math" + "" + d);
     
     }
 }
