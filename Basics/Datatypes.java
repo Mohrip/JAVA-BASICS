9d255a21f0f6aebce2219c65d452f826b269bb83
mohrip <m25srt@gmail.com>
2025-01-20 12:05:24 +0300

    Initial commit of my project

---
+++ Basics/Datatypes.java
@@ -0,0 +1,26 @@
+//package Basics-1;
+
+public class Datatypes {
+   
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    int z = a * b;
+
+    String none = "Hello World";
+    String Fname = "John";
+    String Lname = "Smith";
+    String FullName = Fname + " " + Lname;
+    Boolean isTrue = true;
+
+        System.out.println("This is to print the math" + c + z);
+        System.out.println("This is to print string of none" + none);
+        System.out.println(FullName);
+        System.out.println(Fname + "" + Lname);
+        System.out.println(isTrue);
+    
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,8 @@
+learning 
+
+
+
+## javac *.java / ( is used to compile the whole folder )
+## java {filename} / ( is used to run the file )
+
+## javac -d . *.java / ( is used to compile the whole folder )
