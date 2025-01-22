80ec82c852611441fc77161b3cd5bdca6b2f42d2
mohrip <m25srt@gmail.com>
2025-01-22 10:45:54 +0300

    ""

---
+++ Training/OOP/ClassesAndObjects.java
@@ -0,0 +1,29 @@
+package Training.OOP;
+
+public class ClassesAndObjects {
+
+    public static class Book{
+    private String title;
+    private String author;
+    private String isbn;
+    private double price;
+
+
+    public Book(String title, String author, String isbn, double price) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("Price: " + price);
+    }
+    
+    
+    }
+    
+}
