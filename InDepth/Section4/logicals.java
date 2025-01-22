6db8f534c6b153c695c6704bc499d6a1541f97c9
mohrip <m25srt@gmail.com>
2025-01-22 22:22:50 +0300

    d

--- InDepth/Section4/logicals.java
+++ InDepth/Section4/logicals.java
@@ -2,12 +2,25 @@ package InDepth.Section4;
 
 public class logicals {
     public static void main(String[] args) {
-        int a = 10;
-        boolean b = true;
+        // int a = 10;
+        // boolean b = true;
 
-        if (a > 5 && b) {
-            System.out.println("a is greater than 5 and b is true");
+        // if (a > 5 && b) {
+        //     System.out.println("a is greater than 5 and b is true");
+        // }
+
+
+        double a = 20.00d;
+        double b = 80.00d;
+        double c = (a + b) * 100.00d;
+        double d = c % 40.00d;
+        boolean e = (d == 0) ? true : false;
+        System.out.println(e);
+        if (!e ) {
+            System.out.println("Got some remainder");
         }
+
+
 
 
 
