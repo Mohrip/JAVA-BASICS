9aaddfb51e5c0a0c764be52e3a42029247670e44
mohrip <m25srt@gmail.com>
2025-01-22 15:52:07 +0300

    fix

--- Training/OOP/EmployeesRaise.java
+++ Training/OOP/EmployeesRaise.java
@@ -37,35 +37,21 @@ public class EmployeesRaise {
     }
 
 
-    // public void applySalaryIncrement(double YearPerformance) {
-    //     if (YearPerformance > 3.80 && YearPerformance <= 5) {
-    //         Salary += Salary * (YearPerformance / 100);
-    //         System.out.println("Salary Incremented. New Salary: $" + Salary); }
-
-    //         else if  (YearPerformance > 3 && YearPerformance <= 3.80) {
-    //             Salary += Salary * (YearPerformance / 100);
-    //             System.out.println("Salary Incremented. New Salary: $" + Salary);
-    //         }
-    //      else {
-    //         System.out.println("NO increment for you");
-    //     }
-    // }
+    
     public void evaluatePromotionAndRaise(String NewPosition, double YearPerformance, int YearsOfService) {
-        if (YearPerformance > 4.85 && YearPerformance <= 5) {
+        if (YearPerformance > 4.85 && YearsOfService >= 10  ) {
             Position = NewPosition;
             System.out.println("Promotion to: " + Position);
 
-            // Calculate raise based on years of service
             double raisePercentage;
             if (YearsOfService >= 10) {
-                raisePercentage = 0.20; // 20% raise for 10+ years of service
-            } else if (YearsOfService >= 5) {
-                raisePercentage = 0.10; // 10% raise for 5-9 years of service
+                raisePercentage = 0.20; 
+            } else if (YearsOfService >= 2 && YearPerformance >= 4.5) {
+                raisePercentage = 0.14; 
             } else {
-                raisePercentage = 0.05; // 5% raise for less than 5 years of service
+                raisePercentage = 0.05; 
             }
 
-            // Apply raise to salary
             Salary += Salary * raisePercentage;
             System.out.println("New Salary after raise: SAR" + Salary);
         } else {
@@ -81,6 +67,7 @@ public static void main(String[] args) {
     CompanyInfo Employee1 = new CompanyInfo("IT", "Assosciate Software Engineer", "Riyadh", "Ahmed", 5000, "Mohammed", 1234);
     CompanyInfo Employee2 = new CompanyInfo("HR", "HR Specialist", "Jeddah", "Q", 3000, "Ahmed", 5678);
     CompanyInfo Employee3 = new CompanyInfo("Finance", "Assosciate", "Riyadh", "Khalid", 15000,"Ali", 452);
+    CompanyInfo Employee4 = new CompanyInfo("IT", "Assosciate Software Engineer", "Riyadh", "Ahmed", 6000, "Mohammed", 12345);
 
     System.out.println("Employee 1 Details:");
     Employee1.displayEmployeeDetails();
@@ -94,9 +81,14 @@ public static void main(String[] args) {
     Employee3.displayEmployeeDetails();
     System.out.println();
 
-    Employee1.evaluatePromotionAndRaise("Senior Software Engineer", 5, 3);
-    Employee2.evaluatePromotionAndRaise("Senior HR Specialist", 3.92, 5);
-    Employee3.evaluatePromotionAndRaise("Senior Accountant", 2, 10);
+    System.out.println("Employee 4 Details:");
+    Employee4.displayEmployeeDetails();
+    System.out.println();
+
+    Employee1.evaluatePromotionAndRaise("Senior Software Engineer", 5, 6);
+    Employee2.evaluatePromotionAndRaise("Senior HR Specialist", 4., 5);
+    Employee3.evaluatePromotionAndRaise("Senior Accountant", 2, 7);
+    Employee4.evaluatePromotionAndRaise("Senior Software Engineer", 5, 10);
 
 
     System.out.println();
@@ -110,6 +102,10 @@ public static void main(String[] args) {
     System.out.println();
     System.out.println("Employee 3 Details After Increment:");
     Employee3.displayEmployeeDetails();
+
+    System.out.println();
+    System.out.println("Employee 4 Details After Increment:");
+    Employee4.displayEmployeeDetails();
 
 
 
