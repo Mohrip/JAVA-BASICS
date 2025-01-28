4a2e942879b4a8429f13612fa8cccdca4ddd07a6
mohrip <m25srt@gmail.com>
2025-01-28 21:24:38 +0300

    xxx

--- InDepth/Section6/MoreSwitchs.java
+++ InDepth/Section6/MoreSwitchs.java
@@ -30,5 +30,16 @@ public class MoreSwitchs {
         }
     }
 
+    public static void months(){
+        int month = 1;
+        switch(month){
+            case 1,2,3 -> System.out.println("First quarter");
+            case 4,5,6 -> System.out.println("Second quarter");
+            case 7,8,9 -> System.out.println("Third quarter");
+            case 10,11,12 -> System.out.println("Fourth quarter");
+            default -> System.out.println("Invalid month");
+        }
+    }
+
     
 }
