d19c8d3cdd178f3ab21d118bb7021dbb7196023f
mohrip <m25srt@gmail.com>
2025-02-09 20:06:32 +0300

    p

---
+++ InDepth/Section9/Array.java
@@ -0,0 +1,19 @@
+package InDepth.Section9;
+
+public class Array {
+    public static void main(String[] args)
+   { 
+    int arr[] = {5, 10, 15, 20, 25};
+
+    for (int i =0; i < arr.length; i++)
+    {
+        System.out.println(arr[i]);
+    }
+
+    
+
+
+   }
+
+    
+}
