3095092bbaa6ddc4114ad7002f0c66f08f38d9a0
mohrip <m25srt@gmail.com>
2025-07-21 14:50:31 +0300

    w

---
+++ Training/smallApps/NumberGuessingGame/Main.java
@@ -0,0 +1,9 @@
+package Training.smallApps.NumberGuessingGame;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Guess game = new Guess(0, 1000000000);
+        game.setBounds();
+    }
+}
