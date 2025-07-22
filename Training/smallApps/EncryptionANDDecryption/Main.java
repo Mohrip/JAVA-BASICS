b7b81e9e053560846f52610b4d9a559a33b101ea
mohrip <m25srt@gmail.com>
2025-07-22 12:02:07 +0300

    A

--- Training/smallApps/EncryptionANDDecryption/Main.java
+++ Training/smallApps/EncryptionANDDecryption/Main.java
@@ -1,6 +1,8 @@
 package smallApps.EncryptionANDDecryption;
 
 import javax.crypto.SecretKey;
+import javax.swing.*;
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -13,15 +15,45 @@ public class Main {
             SecretKey key = Encryption.generateKey();
             byte[] iv = Encryption.generateIV();
 
-
             byte[] encryptedData = Encryption.encrypt(data, key, iv);
             System.out.println("Encrypted Data (Base64): " + java.util.Base64.getEncoder().encodeToString(encryptedData));
             System.out.println("IV (Base64): " + java.util.Base64.getEncoder().encodeToString(iv));
 
             String decryptedData = Encryption.decrypt(encryptedData, key, iv);
             System.out.println("Decrypted Data: " + decryptedData);
+
+
+            // 2. File encryption/decryption with hardcoded path
+            String inputFile = "C:\\Users\\m.alsaif\\Desktop\\test22\\test-enc.txt";
+            String encryptedFile = "C:\\Users\\m.alsaif\\Desktop\\test22\\test-enc.enc";
+           // String decryptedFile = "";
+            byte[] fileKey = "12345678901234561234567890123456".getBytes("UTF-8");
+
+            FileEncryption.encryptWitEcb(inputFile, encryptedFile, fileKey);
+           // FileEncryption.decryptWithCbc(encryptedFile, decryptedFile, fileKey);
+            System.out.println("Hardcoded file encrypted and decrypted.");
+
+            // 3. File encryption/decryption with file chooser
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Select a file to encrypt");
+            int result = fileChooser.showOpenDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+                String userEncrypted = selectedFile.getParent() + File.separator + "user_encrypted.enc";
+                String userDecrypted = selectedFile.getParent() + File.separator + "user_decrypted" + getFileExtension(selectedFile);
+
+                FileEncryption.encryptWitEcb(selectedFile.getAbsolutePath(), userEncrypted, fileKey);
+                FileEncryption.decryptWithCbc(userEncrypted, userDecrypted, fileKey);
+                System.out.println("Selected file encrypted and decrypted.");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static String getFileExtension(File file) {
+        String name = file.getName();
+        int lastIndex = name.lastIndexOf('.');
+        return (lastIndex == -1) ? "" : name.substring(lastIndex);
     }
 }
