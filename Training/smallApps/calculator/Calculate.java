3f842b226c2f9e54f1fa67ada091b586071de878
mohrip <m25srt@gmail.com>
2025-08-03 09:50:24 +0300

    aa

--- Training/smallApps/calculator/Calculate.java
+++ Training/smallApps/calculator/Calculate.java
@@ -14,4 +14,41 @@ public class Calculate {
         System.out.println("The result of addition is: " + result);
         return result;
     }
+
+    public double subtraction(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        double firstNumber = scanner.nextDouble();
+        System.out.println("Enter the second number:");
+        double secondNumber = scanner.nextDouble();
+        double result = firstNumber - secondNumber;
+        System.out.println("The result of subtraction is: " + result);
+        return result;
+    }
+
+    public double multiplication() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        double firstNumber = scanner.nextDouble();
+        System.out.println("Enter the second number:");
+        double secondNumber = scanner.nextDouble();
+        double result = firstNumber * secondNumber;
+        System.out.println("The result of multiplication is: " + result);
+        return result;
+    }
+
+    public double division() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        double firstNumber = scanner.nextDouble();
+        System.out.println("Enter the second number:");
+        double secondNumber = scanner.nextDouble();
+        if (secondNumber == 0) {
+            System.out.println("Division by zero is not allowed.");
+            return Double.NaN;
+        }
+        double result = firstNumber / secondNumber;
+        System.out.println("The result of division is: " + result);
+        return result;
+    }
 }
--- Training/smallApps/calculator/Main.java
+++ Training/smallApps/calculator/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Calculate calculator = new Calculate();
         calculator.addition();
-      //  calculator.subtraction();
-       // calculator.multiplication();
-       // calculator.division();
+        calculator.subtraction();
+        calculator.multiplication();
+        calculator.division();
     }
 }
